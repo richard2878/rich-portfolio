b7f42a3bb6f35c11c12658f237a8bdf5894dc6b8
Richard Antolin <richardantolin@gmail.com>
2020-10-17 01:25:37 -0700

    fixed about

--- src/pages/About.js
+++ src/pages/About.js
@@ -62,8 +62,8 @@ function About() {
               My name is Richard Antolin, I am a designer with an background in architecture.  I have learned early on that tools must feel comfortable and easy to use. 
               Otherwise, users will find other tools that will make the job easier to perform.  As a designer, I have responsibilty to provide products that are ageless and long lasting. Wether that is through
               the Apps I've built or the buildings that I have designed, the result should always be that "Form follows Function."  
-              It took a while but I finally decided that I will persue my second career as a Full Stack Developer/UX Engineer. Fast forward, four months later, I have completed a Coding Bootcamp course at
-              UCLA Extension, gained knowledge and learned to love the technicalities of coding. I always knew that I can mix my background in design with building web applications as I have always been interested in figuring out how to make the web experience be more functional and enjoyable.
+              It took a while but I finally decided that I will persue my second career as a Full Stack Developer/UX Engineer. Fast forward, four months later, I have completed a web develpment course at
+              UCLA, I gained knowledge and learned to love the technicalities of web development. I always knew that I can mix my background in design with building web applications as I have always been interested in figuring out how to make the web experience be more functional and enjoyable.
               Please click on my portfolio and see some of the apps I have built through my journey as a coder.  Thanks for visiting.
             </p>
           </Col>
