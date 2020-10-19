16b9991a65ff76d789e8d0a4fc1d3885826a210f
Richard Antolin <richardantolin@gmail.com>
2020-10-19 13:43:46 -0700

    uninstalled unused dependencies

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,8 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-
+ 
+ 
 function About() {
   return (
     <div style={{
