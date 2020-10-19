17959319c85878cb0280032f8f2a3407765ef118
Richard Antolin <richardantolin@gmail.com>
2020-10-19 00:47:08 -0700

    fixed resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ import React from "react";
 function Resume() {
   return (
     <div style={{color:'#37323e', marginTop:'30px', marginLeft:'130px'}}>
-      <center><iframe style={{width:'1000px', height:'1400px', overflow:'auto'}} src='https://docs.google.com/document/d/e/2PACX-1vSPwlbH4bPUvXJBxH8oinOwAKGhEFHDwRKGDqeqZjuWd2b6IrZi6NJdQ109MU2aoo3Hx0KSe4YHPBof/pub?embedded=true'></iframe></center>
+      <center><iframe style={{width:'1000px', height:'1600px', overflow:'auto'}} src='https://docs.google.com/document/d/e/2PACX-1vSPwlbH4bPUvXJBxH8oinOwAKGhEFHDwRKGDqeqZjuWd2b6IrZi6NJdQ109MU2aoo3Hx0KSe4YHPBof/pub?embedded=true'></iframe></center>
     </div>
   );
 }
