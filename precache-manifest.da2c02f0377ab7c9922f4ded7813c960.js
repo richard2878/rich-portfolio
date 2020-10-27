d880b342a3510663c90a113f5af960318b514beb
Richard Antolin <richardantolin@gmail.com>
2020-10-27 11:12:50 -0700

    Updates

---
+++ precache-manifest.da2c02f0377ab7c9922f4ded7813c960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a687f60cf282ca43716d754319a51676",
+    "url": "/rich-portfolio/index.html"
+  },
+  {
+    "revision": "a5f69e5f674b117216fc",
+    "url": "/rich-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1137f3861fc049c0c860",
+    "url": "/rich-portfolio/static/css/main.09ff1a8c.chunk.css"
+  },
+  {
+    "revision": "a5f69e5f674b117216fc",
+    "url": "/rich-portfolio/static/js/2.f89a76ef.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/rich-portfolio/static/js/2.f89a76ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1137f3861fc049c0c860",
+    "url": "/rich-portfolio/static/js/main.9fa9d05d.chunk.js"
+  },
+  {
+    "revision": "77e4536b8149edf091b2",
+    "url": "/rich-portfolio/static/js/runtime-main.9f3ca1fc.js"
+  }
+]);
