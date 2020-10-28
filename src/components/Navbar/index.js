46ab6cc2f6caa30cf31bef308d926a99244f872b
Richard Antolin <richardantolin@gmail.com>
2020-10-27 20:18:10 -0700

    fixed routes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -74,7 +74,7 @@ function Navbar() {
                       marginBottom: '10px'
                     }}
                     to="/about"
-                    className={window.location.pathname === "/rich-portfolio" || 
+                    className={window.location.pathname === "/about" || 
                               window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>
                     About
                   <Info style={{fontSize:'25px', marginLeft:'10px'}}/>
