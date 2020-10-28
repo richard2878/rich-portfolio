089866eb512a6c27ca1026edc184bd5435d44afe
Richard Antolin <richardantolin@gmail.com>
2020-10-27 18:47:38 -0700

    fixed routes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         <div>
           <Navbar />
           <Wrapper>
-            <Route exact path="/" component={About} />
+            <Route exact path="/rich-portfolio" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/email" component={Email} />
